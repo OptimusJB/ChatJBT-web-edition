3d0423a9231dc33e9be108f048fffec7b0ce3626
OptimusJB <110385172+OptimusJB@users.noreply.github.com>
2025-10-26 07:57:21 +0100

    ajout éléments

---
+++ script.js
@@ -0,0 +1,15 @@
+class User{
+    constructor(){
+        this.username = null
+        this.password = null
+    }
+}
+
+function test_click(){
+    alert("bouton cliqué")
+}
+function charger_listeners(){
+    document.getElementById("")
+}
+
+document.addEventListener("DOMContentLoaded", charger_listeners)
