308a32b9335f0056ad76f3744043473a26b6c25b
OptimusJB <110385172+OptimusJB@users.noreply.github.com>
2025-10-26 09:22:10 +0100

    modification du rendu

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ class User{
     }
 }
 
-function test_click(){
-    alert("bouton cliqué")
-}
+
 function charger_listeners(){
     document.getElementById("")
 }
 
 document.addEventListener("DOMContentLoaded", charger_listeners)
+//#476cff
